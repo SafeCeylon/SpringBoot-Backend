561b280a3392ff9250c026b2c62ace3a484851f1
bimsara1999 <blankanatha@gmail.com>
2024-11-27 16:22:37 +0545

    Complete MD officer

--- src/main/java/dev/safeceylon/SafeCeylon/controller/MD_OfficerController.java
+++ src/main/java/dev/safeceylon/SafeCeylon/controller/MD_OfficerController.java
@@ -6,6 +6,8 @@ import dev.safeceylon.SafeCeylon.service.interfac.MD_OfficerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/mdofficer")
@@ -21,4 +23,28 @@ public class MD_OfficerController {
         md_officerService.saveMdofficer(md_officerDTO);
         return "SAVED";
     }
+
+    @PutMapping("/update")
+    public String updateMdofficer(@RequestBody  MD_OfficerDTO md_officerDTO) {
+        md_officerService.updateMdofficer(md_officerDTO);
+        return "UPDATED";
+    }
+
+    @DeleteMapping(
+            path="/delete-officer/{empNumber}"
+    )
+    public String deleteMdofficer(@PathVariable Long empNumber) {
+        md_officerService.deleteMdofficer(empNumber);
+        return "DELETED";
+    }
+
+    @GetMapping(
+            path = "/get-all-officers"
+    )
+
+    public List<MD_OfficerDTO> getAllOfficers(){
+        List<MD_OfficerDTO> allOfficers = md_officerService.getAllOfficers();
+        return allOfficers;
+    }
+
 }
--- src/main/java/dev/safeceylon/SafeCeylon/service/MD_OfficerServiceIMPL.java
+++ src/main/java/dev/safeceylon/SafeCeylon/service/MD_OfficerServiceIMPL.java
@@ -8,6 +8,9 @@ import dev.safeceylon.SafeCeylon.service.interfac.MD_OfficerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MD_OfficerServiceIMPL implements MD_OfficerService {
 
@@ -25,9 +28,67 @@ public class MD_OfficerServiceIMPL implements MD_OfficerService {
                 md_OfficerDTO.getLastName(),
                 md_OfficerDTO.getEmail(),
                 md_OfficerDTO.getContactNumber()
-                //  mdOfficerDTO.getOfficerPhotoUrl()
+                //  md_OfficerDTO.getOfficerPhotoUrl()
         );
         md_OfficerRepo.save(md_Officer);
         return md_OfficerDTO.getFirstName() +  "Successfully Saved";
     }
+
+    @Override
+    public String updateMdofficer(MD_OfficerDTO mdOfficerDTO) {
+        if (md_OfficerRepo.existsById(mdOfficerDTO.getEmpNumber())) {
+            MD_Officer md_Officer = md_OfficerRepo.getReferenceById(mdOfficerDTO.getEmpNumber());
+            md_Officer.setFirstName(mdOfficerDTO.getFirstName());
+            md_Officer.setLastName(mdOfficerDTO.getLastName());
+            md_Officer.setEmail(mdOfficerDTO.getEmail());
+            md_Officer.setContactNumber(mdOfficerDTO.getContactNumber());
+            md_Officer.setNicNumber(mdOfficerDTO.getNicNumber());
+            // md_Officer.setOfficerPhotoUrl(mdOfficerDTO.getEmail());
+
+            md_OfficerRepo.save(md_Officer);
+
+            return mdOfficerDTO.getFirstName() + " Update Success";
+        } else {
+
+            throw new RuntimeException("No data found");
+        }
+    }
+
+    @Override
+    public String deleteMdofficer(Long empNumber) {
+        if(md_OfficerRepo.existsById(empNumber)) {
+            md_OfficerRepo.deleteById(empNumber);
+            return "Successfully Deleted";
+        }else {
+            throw new RuntimeException("No data found");
+        }
+    }
+
+    @Override
+    public List<MD_OfficerDTO> getAllOfficers() {
+
+        List<MD_Officer> getAllOfficers = md_OfficerRepo.findAll();
+        List<MD_OfficerDTO> officerDTOList = new ArrayList<>();
+
+        for(MD_Officer md_Officer : getAllOfficers) {
+
+            MD_OfficerDTO md_officerDTO = new MD_OfficerDTO (
+
+                    md_Officer.getEmpNumber(),
+                    md_Officer.getNicNumber(),
+                    md_Officer.getFirstName(),
+                    md_Officer.getLastName(),
+                    md_Officer.getEmail(),
+                    md_Officer.getContactNumber()
+                    // md_Officer.getOfficerPhotoUrl()
+            );
+            officerDTOList.add(md_officerDTO);
+        }
+        return officerDTOList;
+
+    }
+
+
 }
+
+
