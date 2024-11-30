79eb4474526b2c71cd1299f03714996769fa7c39
bimsara1999 <blankanatha@gmail.com>
2024-11-30 12:13:58 +0545

    Update MD officer entity

--- src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficer.java
+++ src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficer.java
@@ -1,4 +1,38 @@
 package dev.safeceylon.SafeCeylon.metreologtdepartmentofficer;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = " metreologydepartmentofficer")
+
 public class MetreologyDepartmentOfficer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long empNumber;
+
+    @Column(nullable = false , unique = true)
+    private String nicNumber;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String contactNumber;
+
+     @Column(name = "photo", nullable = false)
+     private String officerPhotoUrl;
+
 }
