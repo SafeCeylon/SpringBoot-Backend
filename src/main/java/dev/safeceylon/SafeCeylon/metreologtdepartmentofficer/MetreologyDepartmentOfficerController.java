df1a722d32b7b13557c6da5b888f98b879d0ed21
bimsara1999 <blankanatha@gmail.com>
2024-11-30 15:26:07 +0545

    Complete CRUD for MD Officer

--- src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficerController.java
+++ src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficerController.java
@@ -1,7 +1,10 @@
 package dev.safeceylon.SafeCeylon.metreologtdepartmentofficer;
+import dev.safeceylon.SafeCeylon.disastermanagement.Disaster;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/mdofficer")
@@ -23,4 +26,23 @@ import org.springframework.web.bind.annotation.*;
         MetreologyDepartmentOfficer savedMdofficer = metreologyDepartmentOfficerService.saveMdofficer(metreologyDepartmentOfficer);
         return ResponseEntity.ok(savedMdofficer);
     }
+
+    @GetMapping
+    public ResponseEntity<List<MetreologyDepartmentOfficer>> getAllOfficers() {
+        List<MetreologyDepartmentOfficer> metreologyDepartmentOfficers = metreologyDepartmentOfficerService.getAllOfficers();
+        return ResponseEntity.ok(metreologyDepartmentOfficers);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MetreologyDepartmentOfficer> updateMdofficer(@PathVariable Long id, @RequestBody MetreologyDepartmentOfficer metreologyDepartmentOfficer) throws IllegalAccessException {
+        MetreologyDepartmentOfficer updateMdofficer = metreologyDepartmentOfficerService.updateMdofficer(id, metreologyDepartmentOfficer);
+        return ResponseEntity.ok(updateMdofficer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MetreologyDepartmentOfficer> deleteMdofficer(@PathVariable Long id) {
+        metreologyDepartmentOfficerService.deleteOfficer(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficerService.java
+++ src/main/java/dev/safeceylon/SafeCeylon/metreologtdepartmentofficer/MetreologyDepartmentOfficerService.java
@@ -1,10 +1,14 @@
 package dev.safeceylon.SafeCeylon.metreologtdepartmentofficer;
 
+import dev.safeceylon.SafeCeylon.disastermanagement.Disaster;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MetreologyDepartmentOfficerService {
+
     @Autowired
     private MetreologyDepartmentOfficerRepo metreologyDepartmentOfficerRepo;
 
@@ -12,5 +16,31 @@ public class MetreologyDepartmentOfficerService {
     public MetreologyDepartmentOfficer saveMdofficer(MetreologyDepartmentOfficer metreologyDepartmentOfficer) {
 
         return metreologyDepartmentOfficerRepo.save(metreologyDepartmentOfficer);
+    }
+
+    public List<MetreologyDepartmentOfficer> getAllOfficers(){
+        return metreologyDepartmentOfficerRepo.findAll();
+    }
+
+
+    public MetreologyDepartmentOfficer updateMdofficer(Long id, MetreologyDepartmentOfficer metreologyDepartmentOfficer) throws  IllegalAccessException {
+
+        MetreologyDepartmentOfficer existingMdofficer = metreologyDepartmentOfficerRepo.findById(id).orElseThrow(
+                () -> new IllegalAccessException("Disaster not found")
+        );
+        existingMdofficer.setEmpNumber(metreologyDepartmentOfficer.getEmpNumber());
+        existingMdofficer.setNicNumber(metreologyDepartmentOfficer.getNicNumber());
+        existingMdofficer.setFirstName(metreologyDepartmentOfficer.getFirstName());
+        existingMdofficer.setLastName(metreologyDepartmentOfficer.getLastName());
+        existingMdofficer.setEmail(metreologyDepartmentOfficer.getEmail());
+        existingMdofficer.setContactNumber(metreologyDepartmentOfficer.getContactNumber());
+        existingMdofficer.setOfficerPhotoUrl(metreologyDepartmentOfficer.getOfficerPhotoUrl());
+
+
+        return metreologyDepartmentOfficerRepo.save(existingMdofficer);
+    }
+
+    public void deleteOfficer(Long id) {
+        metreologyDepartmentOfficerRepo.deleteById(id);
     }
 }
